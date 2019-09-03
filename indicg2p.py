c65ffe69a62ecc322592d465689cf6006d583093
skolachi <sudheer.kpg08@gmail.com>
2019-09-03 16:16:51 +0100

    latest

--- indicg2p.py
+++ indicg2p.py
@@ -7,7 +7,7 @@ language_prefixes = {'dev':'09','telugu':'0C','gurmukhi':'0A','malayalam':'0D'}
 
 def extract_charmap(filename='indicg2p.map',lang='dev'):
     g2pmap = {}
-    chars = [f.strip().split() for f in open(filename)][1:]
+    chars = [f.strip().split(',') for f in open(filename)][1:]
     lang_chars = [language_prefixes[lang]+c[1][2:] for c in chars]
     for c1,c2 in zip(chars,lang_chars):
         if c1[0] == 'NULL':c1[0] = ''
@@ -17,7 +17,11 @@ def extract_charmap(filename='indicg2p.map',lang='dev'):
 def group_charmap(charmap):
     charmap_groups = {}
     charmap_groups['maatra'] = []
+    charmap_groups['diacritic'] = []
     for k in charmap.keys():
+        print(name(k[0]))
+        if 'ANUSVARA' in name(k[0]).split() or 'NUKTA' in name(k[0]).split():
+            charmap_groups['diacritic'].append(k)
         if 'SIGN' in name(k[0]).split():
             charmap_groups['maatra'].append(k)
 
@@ -31,10 +35,14 @@ def convert(text,lang):
         if k not in charmap_groups['maatra']:
             text = text.replace(k[1],charmap[k])
 
-    for k in charmap_groups['maatra']:
+    for k in charmap_groups['diacritic']:
         if charmap[k] == 'M':
-            text = text.replace('%s'%(k[1]),charmap[k].split(',')[0])
-        else:
+            text = text.replace('%s'%(k[1]),charmap[k])
+        if charmap[k] == 'za':
+            text = text.replace('%s'%('a'+k[1]),charmap[k])
+
+    for k in charmap_groups['maatra']:
+        if k not in charmap_groups['diacritic']:
             text = text.replace('%s'%('a'+k[1]),charmap[k])
                 
     return text
